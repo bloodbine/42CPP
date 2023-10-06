649e8d8dfaf0f3b712ca02923f45db350a7c4c1c
Gergely Pasztor <gpasztor@3-G-11.42heilbronn.de>
2023-10-06 10:42:48 +0200

    Gonna partially rewrite project

--- CPP03/ex03/DiamondTrap.cpp
+++ CPP03/ex03/DiamondTrap.cpp
@@ -6,15 +6,16 @@
 /*   By: gpasztor <gpasztor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:45:25 by gpasztor          #+#    #+#             */
-/*   Updated: 2023/10/04 16:10:20 by gpasztor         ###   ########.fr       */
+/*   Updated: 2023/10/06 10:32:34 by gpasztor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(): ClapTrap(), ScavTrap(), FragTrap(), _name("DefDiaTrap")
+DiamondTrap::DiamondTrap(): ClapTrap(), ScavTrap(), FragTrap()
 {
-	this->ClapTrap::setName(this->getName() + "_clap_name");
+	this->_name = "DefTrap";
+	this->ClapTrap::_name = this->_name + "_clap_name";
 	this->setHP(this->FragTrap::getHP());
 	this->setEP(this->ScavTrap::getEP());
 	this->setAP(this->FragTrap::getAP());
@@ -28,4 +29,40 @@ DiamondTrap::DiamondTrap(std::string name): ClapTrap(), ScavTrap(), FragTrap(), 
 	this->setEP(this->ScavTrap::getEP());
 	this->setAP(this->FragTrap::getAP());
 	std::cout << "DiamondTrap unit " << this->getName() << " has been constructed" << std::endl;
+};
+
+DiamondTrap::~DiamondTrap()
+{
+	std::cout << "DiamondTrap unit " << this->getName() << " has been destructed" << std::endl;
+};
+
+DiamondTrap::DiamondTrap(const DiamondTrap &obj): ClapTrap(obj), ScavTrap(obj), FragTrap(obj)
+{
+	std::cout << "DiamondTrap copy constructor called" << std::endl;
+	*this = obj;
+};
+
+DiamondTrap& DiamondTrap::operator=(const DiamondTrap &obj)
+{
+	if (this != &obj)
+	{
+		this->setName(obj.getName());
+		this->ClapTrap::setName(obj.ClapTrap::getName());
+		this->setHP(obj.getHP());
+		this->setEP(obj.getEP());
+		this->setAP(obj.getAP());
+		std::cout << "DiamondTrap unit " << this->getName() << " has been constructed" << std::endl;
+	}
+	return (*this);
+};
+
+void	DiamondTrap::attack(const std::string& target)
+{
+	this->ScavTrap::attack(target);
+};
+
+void	DiamondTrap::whoAmI()
+{
+	std::cout << "Hi, I am DiamondTrap unit " << this->_name\
+	<< ", originally made as ClapTrap unit " << ClapTrap::_name << std::endl;
 };
--- CPP03/ex03/main.cpp
+++ CPP03/ex03/main.cpp
@@ -6,20 +6,20 @@
 /*   By: gpasztor <gpasztor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 15:01:10 by gpasztor          #+#    #+#             */
-/*   Updated: 2023/10/03 16:39:00 by gpasztor         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:23:50 by gpasztor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int	main(void)
 {
-	FragTrap	Fraggie("Fraggie");
+	DiamondTrap	BestTrap;
 
-	Fraggie.attack("Random raider");
-	Fraggie.takeDamage(5);
-	Fraggie.beRepaired(5);
-	Fraggie.takeDamage(10);
-	Fraggie.highFivesGuys();
-	Fraggie.takeDamage(200);
+	BestTrap.attack("Random raider");
+	BestTrap.takeDamage(5);
+	BestTrap.beRepaired(5);
+	BestTrap.takeDamage(10);
+	BestTrap.whoAmI();
+	BestTrap.takeDamage(200);
 };
