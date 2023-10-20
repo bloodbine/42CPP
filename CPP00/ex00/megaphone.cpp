1f72c9e846878780251fddbc405900afd2c5e78b
Gergely Pasztor <gpasztor@3-G-11.42heilbronn.de>
2023-10-20 11:41:57 +0200

    C++-ified CPP00/ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,32 +6,28 @@
 /*   By: gpasztor <gpasztor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 16:19:16 by gpasztor          #+#    #+#             */
-/*   Updated: 2023/08/08 16:19:19 by gpasztor         ###   ########.fr       */
+/*   Updated: 2023/10/20 11:40:49 by gpasztor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 int	main(int argc, char **argv)
 {
-	int	i = 0, j = 1;
-
-	if (argc == 1)
+	if (argc > 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		for (int i = 1; i < argc; i++)
+		{
+			for (size_t j = 0; j < std::strlen(argv[i]); j++)
+			{
+				std::cout << (char)(std::toupper(argv[i][j]));
+			}
+			std::cout << " ";
+		}
+		std::cout << std::endl;
 		return (0);
 	}
-	while (j < argc)
-	{
-		while (argv[j][i] != '\0')
-		{
-			std::cout << (char)std::toupper(argv[j][i]);
-			i++;
-		}
-		i = 0;
-		j++;
-	}
-	std::cout << std::endl;
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return (0);
 }
